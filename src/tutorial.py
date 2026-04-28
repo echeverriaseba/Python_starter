0add803da5c20928a20a10a0293c76985e221a1b
echeverriaseba <secheverriap@gmail.com>
2026-04-28 15:55:08 +0200

    creating folders

---
+++ src/tutorial.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+def hello():
+    """Print "Hello World" and return None."""
+    print("Hello World")
+
+    
+# Main program starts here
+hello()
+
+dir() # Inspecting objects defined in the Console
+
+help(hello) # Returns the additional information included as documentation string in the function
