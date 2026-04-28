d1933ded563e31659b11142c71fa5f9d1a6e6f0b
echeverriaseba <secheverriap@gmail.com>
2026-04-28 16:18:02 +0200

    first code commit

--- src/tutorial.py
+++ src/tutorial.py
@@ -5,14 +5,33 @@ Spyder Editor
 This is a temporary script file.
 """
 
+# 1. Defining a function ####
+
 def hello():
     """Print "Hello World" and return None."""
     print("Hello World")
 
-    
-# Main program starts here
+
 hello()
 
 dir() # Inspecting objects defined in the Console
 
 help(hello) # Returns the additional information included as documentation string in the function
+
+# 1. Automatic Symbolic Python ####
+# after installing SymPy Python package with:
+# conda install SymPy
+# import SymPy
+
+# e.g. 1:
+expr = (x + y) ** 3
+expr # returns the previous expression in LaTex math syntax
+
+expr.expand() # opens the polynome    
+    
+# e.g. 2:   
+expr2 = (x + y + z) ** 3
+expr2 # returns the previous expression in LaTex math syntax
+
+expr2.expand() # opens the polynome
+
