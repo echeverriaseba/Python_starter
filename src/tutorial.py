ab426d797fa20933f1183554c36b71402d5edc87
echeverriaseba <secheverriap@gmail.com>
2026-05-14 13:44:10 +0200

    further commands

--- src/tutorial.py
+++ src/tutorial.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
-
-This is a temporary script file.
+This script goes through basic Python commands and synthax. ¡
 """
 
 # %% 1. Defining functions 
@@ -81,6 +79,7 @@ print(result)
 globals()
 
 # %% 4. Debugging
+
 # When running a cell (Ctrl+Enter) the console shows only the final result of all lines within a cell. In order to look for mistakes("bugs") we open the debugging mode with Ctrl+F5. Let's say we have:
     
 def average(a,b):
@@ -98,13 +97,69 @@ print(avg) # prints 7.0 intead of 5.0
 # - open the debugger console (ipdb>)
 # use debug commands n, s, c, and p to identify the error and correct the function
  
+type(avg) #  A float in Python is a data type used to represent numbers that have a decimal point
+type(a) # Int, or integer, is a whole number, positive or negative, without decimals, of unlimited length.
+# %% 5. The math library
 
-# %% 5. Working with data using NumPy package 
-import numpy as np # imports the NumPy package (similar to R's library()) and assigns it the "np" labell
+import math # the math library contains basic math equations like sin() and sqrt(), and values such as pi().
+math.sin(3)
+math.sqrt(25)
+math.sqrt(math.pi)
+
+# %% 6. Creating and using lists
+
+station_names = [
+    "Helsinki Harmaja",
+    "Helsinki Kaisaniemi",
+    "Helsinki Kaivopuisto",
+    "Helsinki Kumpula",
+]
+
+print(station_names) # use from now on to check the current state of the list
+
+len(station_names) # analogue to R's lenght()
+
+station_names[1] # just as in R, list elements can be accessed with their index values. Note here again that, as Python starts values stored in collections with the index value 0, this returns the second value.
+type(station_names[1])
+
+station_names[3] = "modified value" # edit a value within the list
+
+station_names.append("new value") # add a new value to the list
+
+del station_names[2] # removing a list value by its index
+
+station_names.remove("new value") # removing a list value by its name
+
+station_names.count("Helsinki Harmaja") # checking how many times a value is contained in a list
+
+station_names.index("Helsinki Harmaja") # checkin a value's index within a list
+
+station_names.reverse() # to reverse the order of items in a list
+
+# %% 7. Working with text and numbers
+# Station information
+station_name = "Helsinki Kaivopuisto" # type(station_name) = str
+station_id = 132310 # type(station_id) = int
+temp = 18.56789876 # type(temp) = float
+
+# The f-string approach
+# Here the f character activates the f-string functionality
+# The .2f is the Format specifier (precision and type), without it then the entire temp value is printed.
+info_text = (
+    f"The temperature at {station_name} (ID: {station_id}) is {temp:.2f} Celsius."
+)
+print(info_text)
+
+text = "Stations: Helsinki Kumpula, Helsinki Kaisaniemi, Helsinki Harmaja"
+
+# %% 8. Manipulating character strings 
+# %% 9. Working with data using NumPy package 
+import numpy as np # imports the NumPy package (similar to R's library()) and assigns it the "np" label
 
 a = np.arange(15).reshape(3, 5) 
-a 
+a # note that in Python vectors and matrixes start from 0, not 1 as in R.
 
+help(np)
 
 
 
